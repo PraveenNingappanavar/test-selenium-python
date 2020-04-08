52d875d78d1e252b17939fdc35268ea9f58cf4ad
Andrii Baidachenko <andrewbayd@gmail.com>
2020-04-08 22:08:02 +0200

    create file with locators

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,8 +1,12 @@
 from selenium.webdriver.common.by import By
 
 from pages.base_page import BasePage
+from pages.locators import MainPageLocators
 
 
 class MainPage(BasePage):
     def go_to_login_page(self):
-        self.browser.find_element(By.CSS_SELECTOR, "#login_link").click()
+        self.browser.find_element(*MainPageLocators.LOGIN_LINK).click()
+
+    def should_have_login_link(self):
+        assert self.browser.find_element(*MainPageLocators.LOGIN_LINK).is_displayed(), "Login link is not displayed"
--- test_main_page.py
+++ test_main_page.py
@@ -3,6 +3,13 @@ from pages.main_page import MainPage
 URL = "http://selenium1py.pythonanywhere.com/"
 
 
+def test_main_page_has_login_link(browser):
+    main_page = MainPage(browser, URL)
+
+    main_page.open()
+    main_page.should_have_login_link()
+
+
 def test_guest_can_open_login_page(browser):
     main_page = MainPage(browser, URL)
 
