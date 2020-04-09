2bc983fbe95fce71261dc111d0fe32ca4ac41a81
Andrii Baidachenko <andrewbayd@gmail.com>
2020-04-09 13:24:19 +0200

    create login page

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,6 +1,22 @@
+from selenium.webdriver.common.by import By
+
 from .base_page import BasePage
+from .locators import LoginPageLocators
 
 
 class LoginPage(BasePage):
     def should_be_login_page(self):
-        return "login" in self.browser.current_url()
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        assert "login" in self.browser.current_url
+
+    def should_be_login_form(self):
+        login_form = self.browser.find_element(By.CSS_SELECTOR, LoginPageLocators.LOGIN_FORM)
+        assert login_form.is_displayed(), "Login form is not displayed"
+
+    def should_be_register_form(self):
+        register_form = self.browser.find_element(By.CSS_SELECTOR, LoginPageLocators.REGISTER_FORM)
+        assert register_form.is_displayed(), "Register form is not displayed"
