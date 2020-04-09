861e6c95082da74c6bbe270a1f2240339953ef91
Andrii Baidachenko <andrewbayd@gmail.com>
2020-04-09 15:33:54 +0200

    add logging to product page

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,3 +1,6 @@
+import math
+
+from selenium.common.exceptions import NoAlertPresentException
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
@@ -15,3 +18,17 @@ class BasePage:
         print(f"..trying to find element by locator {locator}")
         return WebDriverWait(self.browser, time).until(EC.presence_of_element_located(locator),
                                                        f"Can't find element by locator {locator}")
+
+    def solve_quiz_and_get_code(self):
+        alert = self.browser.switch_to.alert
+        x = alert.text.split(" ")[2]
+        answer = str(math.log(abs((12 * math.sin(float(x))))))
+        alert.send_keys(answer)
+        alert.accept()
+        try:
+            alert = self.browser.switch_to.alert
+            alert_text = alert.text
+            print(f"Your code: {alert_text}")
+            alert.accept()
+        except NoAlertPresentException:
+            print("No second alert presented")
--- pages/product_page.py
+++ pages/product_page.py
@@ -4,16 +4,25 @@ from .locators import ProductPageLocators
 
 class ProductPage(BasePage):
     def add_product_to_cart(self):
+        print("Adding product to cart")
         self.find_element(ProductPageLocators.ADD_TO_CART_BUTTON).click()
 
     def get_product_name(self):
-        return self.find_element(ProductPageLocators.PRODUCT_NAME).text
+        print("Getting product name")
+        product_name = self.find_element(ProductPageLocators.PRODUCT_NAME).text
+        print(f"Product name is {product_name}")
+        return product_name
 
     def get_product_price(self):
-        return self.find_element(ProductPageLocators.PRODUCT_PRICE).text
+        print("Getting product price")
+        product_price = self.find_element(ProductPageLocators.PRODUCT_PRICE).text
+        print(f"Product price is {product_price}")
+        return product_price
 
     def product_success_message_should_have(self, product_name):
+        print(f"Checking that {product_name} is displayed in success message")
         assert product_name in self.find_element(ProductPageLocators.PRODUCT_ADDED_SUCCESS_MESSAGE).text
 
     def basket_total_should_be_equal(self, product_price):
+        print(f"Checking that basket total equals {product_price}")
         assert product_price in self.find_element(ProductPageLocators.BASKET_TOTAL_MESSAGE).text
